b8ec375f5a87820a6b658e450c9aca627df60c89
JackTsai <jacktsai@larvata.tw>
2021-04-17 17:24:26 +0800

    feat: 增加 Larvata::Mechanisms.association_between_user_and_role 參數來設定 UserRole 類別的 table_name。

--- app/models/larvata/mechanisms/concerns/models/user_role.rb
+++ app/models/larvata/mechanisms/concerns/models/user_role.rb
@@ -2,7 +2,7 @@ module Larvata::Mechanisms::Concerns::Models::UserRole
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :role, optional: true
-    belongs_to :user, optional: true
+    belongs_to :role, class_name: 'Larvata::Mechanisms::Role', optional: true
+    belongs_to :user, class_name: 'Larvata::Mechanisms::User', optional: true
   end
 end
--- lib/larvata/mechanisms.rb
+++ lib/larvata/mechanisms.rb
@@ -4,5 +4,8 @@ module Larvata
   module Mechanisms
     mattr_accessor :table_name_prefix
     @@table_name_prefix = nil
+
+    mattr_accessor :association_between_user_and_role
+    @@association_between_user_and_role = 'users_roles'
   end
 end
