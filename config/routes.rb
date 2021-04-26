6d3fa8abb3fe9ed6a71d3cdd76626093ae76d2b0
JackTsai <jacktsai@larvata.tw>
2021-04-26 15:52:37 +0800

    chore: 調整 devise 設定以及錯誤頁面回首頁的連結設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Larvata::Mechanisms::Engine.routes.draw do
-  devise_for :users, class_name: "Larvata::Mechanisms::User"
-
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
 end
