c93338a4f0af4e263b892f64aae662fc89ff9e1c
JackTsai <jacktsai@larvata.tw>
2021-07-19 21:20:45 +0800

    feat: 增加 log 錯誤訊息機制。

--- app/controllers/larvata/mechanisms/errors_controller.rb
+++ app/controllers/larvata/mechanisms/errors_controller.rb
@@ -1,11 +1,26 @@
 class Larvata::Mechanisms::ErrorsController < ApplicationController
   layout "larvata/mechanisms/errors"
 
+  before_action :handle_exception
+  before_action :log_exception_messages
+
   def not_found
     render status: 404
   end
 
   def internal_server_error
     render status: 500
+  end
+
+  private
+
+  def handle_exception
+    @exception = request.env["action_dispatch.exception"]
+    @exception_wrapper = ActionDispatch::ExceptionWrapper.new(request.env['action_dispatch.backtrace_cleaner'], @exception)
+    @trace = @exception_wrapper.application_trace
+  end
+
+  def log_exception_messages
+    logger.error "exception：#{@exception}，paramters：#{request.params}，application trace：#{@trace.join('；')}"
   end
 end
