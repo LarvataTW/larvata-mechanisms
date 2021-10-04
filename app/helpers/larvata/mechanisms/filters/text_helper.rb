121a7ffd085e18a9388bf98824d3ff229de767e4
JackTsai <jacktsai@larvata.tw>
2021-10-04 10:51:54 +0800

    feat: 增加文字查詢框 helper。

---
+++ app/helpers/larvata/mechanisms/filters/text_helper.rb
@@ -0,0 +1,11 @@
+module Larvata::Mechanisms::Filters::TextHelper
+  # label: 此查詢欄位的 label 名稱
+  # class_name: 此查詢欄位的 class 設定
+  # column_name: 此查詢欄位名稱
+  def text_filter_tag(label: '', class_name: '', column_name: '')
+    content_tag(:div, class: 'form-group') do
+      label_tag(label, nil, class: 'bmd-label-floating') +
+        text_field_tag("#{column_name}", nil, class: "form-control #{class_name} filter-#{column_name} filter-condition", autocomplete: 'off')
+    end
+  end
+end
