ed27d45e1e63b00e7bf55ce7d622d98c3c352ab1
JackTsai <jacktsai@larvata.tw>
2021-12-22 13:53:08 +0800

    fix: 調整 下拉選單、radio_button、check_box 等輸入元件的 layout 結構。

--- app/helpers/larvata/mechanisms/inputs/collection_helper.rb
+++ app/helpers/larvata/mechanisms/inputs/collection_helper.rb
@@ -9,19 +9,21 @@ module Larvata::Mechanisms::Inputs::CollectionHelper
   # input_class_name: 項目的 class
   # disabled: 是否禁用，預設為 false
   def radio_buttons_tag(form:, label:, field_name:, collection: [], value_method: :first, text_method: :last, value: nil, input_class_name: '', disabled: false)
-    content_tag(:div, class: 'form-group bmd-form-group is-filled') do
-      label_part = label_tag(label, nil, class: 'bmd-label-floating')
-      radio_part = form.collection_radio_buttons field_name, collection, value_method, text_method,
-                                                 include_hidden: false, checked: value,
-                                                 item_wrapper_class: "radio-inline" do |input|
-        input.label(class: "radio-inline mr-3") {
-          input.radio_button(disabled: disabled, class: "#{input_class_name} mr-1") + input.text
-        }
+    content_tag(:div, class: 'bmd-form-group') do
+      content_tag(:div, class: 'form-group is-filled') do
+        label_part = label_tag(label, nil, class: 'bmd-label-floating')
+        radio_part = form.collection_radio_buttons field_name, collection, value_method, text_method,
+                                                   include_hidden: false, checked: value,
+                                                   item_wrapper_class: "radio-inline" do |input|
+          input.label(class: "radio-inline mr-3") {
+            input.radio_button(disabled: disabled, class: "#{input_class_name} mr-1") + input.text
+          }
+        end
+
+        error_part = form.full_error field_name, class: 'text-danger'
+
+        label_part + radio_part + error_part
       end
-
-      error_part = form.full_error field_name, class: 'text-danger'
-
-      label_part + radio_part + error_part
     end
   end
 
@@ -35,19 +37,21 @@ module Larvata::Mechanisms::Inputs::CollectionHelper
   # input_class_name: 項目的 class
   # disabled: 是否禁用，預設為 false
   def check_boxes_tag(form:, label:, field_name:, collection: [], value_method: :first, text_method: :last, value: nil, input_class_name: '', disabled: false)
-    content_tag(:div, class: 'form-group bmd-form-group is-filled') do
-      label_part = label_tag(label, nil, class: 'bmd-label-floating')
-      check_box_part = form.collection_check_boxes field_name, collection, value_method, text_method,
-                                               include_hidden: false, checked: value,
-                                               item_wrapper_class: "checkbox-inline" do |input|
-        input.label(class: "radio-inline mr-3") {
-          input.check_box(disabled: disabled, class: "#{input_class_name} mr-1") + input.text
-        }
+    content_tag(:div, class: 'bmd-form-group') do
+      content_tag(:div, class: 'form-group is-filled') do
+        label_part = label_tag(label, nil, class: 'bmd-label-floating')
+        check_box_part = form.collection_check_boxes field_name, collection, value_method, text_method,
+                                                     include_hidden: false, checked: value,
+                                                     item_wrapper_class: "checkbox-inline" do |input|
+          input.label(class: "radio-inline mr-3") {
+            input.check_box(disabled: disabled, class: "#{input_class_name} mr-1") + input.text
+          }
+        end
+
+        error_part = form.full_error field_name, class: 'text-danger'
+
+        label_part + check_box_part + error_part
       end
-
-      error_part = form.full_error field_name, class: 'text-danger'
-
-      label_part + check_box_part + error_part
     end
   end
 
@@ -64,23 +68,25 @@ module Larvata::Mechanisms::Inputs::CollectionHelper
   def select2_tag(form:, label:, field_name:, collection: [], value_method: :first, text_method: :last, input_class_name: 'select2', value: '', disabled: false, multiple: false, allow_clear: true, include_blank: false)
     allow_clear_class = allow_clear ? 'allow-clear' : ''
 
-    content_tag(:div, class: 'form-group bmd-form-group is-filled') do
-      label_part = label_tag(label, nil, class: 'bmd-label-floating')
-      select_part = form.collection_select field_name, collection, value_method, text_method,
-                                           {
-                                             include_hidden: false,
-                                             include_blank: include_blank,
-                                             selected: value
-                                           },
-                                           {
-                                             class: "form-control #{input_class_name} #{allow_clear_class}",
-                                             multiple: multiple,
-                                             disabled: disabled
-                                           }
+    content_tag(:div, class: 'bmd-form-group') do
+      content_tag(:div, class: 'form-group is-filled') do
+        label_part = label_tag(label, nil, class: 'bmd-label-floating')
+        select_part = form.collection_select field_name, collection, value_method, text_method,
+                                             {
+                                               include_hidden: false,
+                                               include_blank: include_blank,
+                                               selected: value
+                                             },
+                                             {
+                                               class: "form-control #{input_class_name} #{allow_clear_class}",
+                                               multiple: multiple,
+                                               disabled: disabled
+                                             }
 
-      error_part = form.full_error field_name, class: 'text-danger'
+        error_part = form.full_error field_name, class: 'text-danger'
 
-      label_part + select_part + error_part
+        label_part + select_part + error_part
+      end
     end
   end
 
